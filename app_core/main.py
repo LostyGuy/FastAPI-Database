40a12edddba8889d7da694b3581bd192141342f1
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-04-03 22:41:40 +0200

    fix: correct FastAPI import and update model imports; remove empty template files

--- app_core/main.py
+++ app_core/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPIm, Request, Depends
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
